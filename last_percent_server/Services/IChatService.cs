947e4b1b5f5962edb764f6a192a2f4363b62207f
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-04-05 12:44:04 +0300

    Server -> created ichat service

---
+++ last_percent_server/Services/IChatService.cs
@@ -0,0 +1,8 @@
+using last_percent_server.Models;
+
+namespace last_percent_server.Services;
+
+public interface IChatService
+{
+    Task<IEnumerable<Message>> GetMatchMessagesAsync(int matchId, int userId);
+}
