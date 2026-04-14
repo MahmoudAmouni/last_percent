20e97607e756a938c038b886fcb0b8cd212d84d1
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-04-14 12:46:12 +0300

    Server -> removing signalR and using socketio

---
+++ last_percent_server/WebSockets/ISocketManager.cs
@@ -0,0 +1,11 @@
+using System.Net.WebSockets;
+
+namespace last_percent_server.WebSockets;
+
+public interface ISocketManager
+{
+    void AddSocket(int userId, WebSocket socket);
+    void RemoveSocket(int userId);
+    Task SendAsync(int userId, object payload);
+    Task BroadcastAsync(IEnumerable<int> userIds, object payload);
+}
